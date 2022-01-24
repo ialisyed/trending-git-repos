dd17df0a7b83d8c97b47e893ea30b0decb8871d9
Syed Saeed Ul Hassan Ali <ialisyed96@gmail.com>
2022-01-24 21:56:41 +0100

    Removing counter feature

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,9 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import reposReducer from "../features/repos/reposSlice";
-import counterReducer from "../features/counter/counterSlice";
 
 export const store = configureStore({
   reducer: {
     repos: reposReducer,
-    counter: counterReducer,
   },
 });
 
--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
-import Header from "./layout/Header";
 
 ReactDOM.render(
   <React.StrictMode>
